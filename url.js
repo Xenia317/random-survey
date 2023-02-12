c232c48580438bed144c1b7591ab47f01a3bd1a6
Xenia317 <120579848+Xenia317@users.noreply.github.com>
2023-02-12 23:15:26 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,14 +9,10 @@
 
 const urls = [
 	'https://www.surveycake.com/s/bpZpr',
-        'https://www.surveycake.com/s/X6P6p',
         'https://www.surveycake.com/s/mZ7Zr',
-        'https://www.surveycake.com/s/zZkZX',
         'https://www.surveycake.com/s/Z6Z6q',
 	'https://www.surveycake.com/s/Y6ZyK',
-	'https://www.surveycake.com/s/xDBlp',
 	'https://www.surveycake.com/s/7QDbv',
 	'https://www.surveycake.com/s/k6x6m',
-	'https://www.surveycake.com/s/NW71e',
 	'https://www.surveycake.com/s/xDBlp'
 ];
